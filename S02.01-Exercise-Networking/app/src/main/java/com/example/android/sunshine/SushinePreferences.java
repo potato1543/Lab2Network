f2f0825fc571c255135d06414df546382936f96f
potato1543 <kaila9511@gmail.com>
2017-06-18 21:50:04 -0700

    Network Sunshine
    Network

---
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/SushinePreferences.java
@@ -0,0 +1,12 @@
+package com.example.android.sunshine;
+
+/**
+ * Created by Owner on 6/18/2017.
+ */
+
+class SushinePreferences {
+    public static final String PREF_CITY_NAME = "San Gabriel";
+    private static final String DEFAULT_WEATHER_LOCATION = "91776,USA";
+    private static final String DEFAULT_MAP_LOCATION =
+            "204 East Broadway Ave San Gabriel,CA 91776";
+}
